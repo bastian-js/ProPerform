4f5c949a3241d51dabefe83fccae1cf9de29a0ba
b-bastian <bbrugger@schueler.hakzell.at>
2026-02-28 20:14:42 +0100

    added imports on server

--- routes/TrainerRoutes/private/linkAthleteRoutes.js
+++ routes/TrainerRoutes/private/linkAthleteRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { db } from "../../../db.js";
 import { createRateLimiter } from "../../../middleware/rate.js";
+import { requireAuth } from "../../../middleware/auth.js";
+import { requireRole } from "../../../middleware/role.js";
 
 const router = express.Router();
 

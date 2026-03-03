ec453e0e1645c7cd779bff809a2bbf2dc2bcfab7
b-bastian <bbrugger@schueler.hakzell.at>
2026-03-03 11:51:58 +0100

    updated the response in verify token

--- routes/AuthRoutes/protectedAuthRoutes.js
+++ routes/AuthRoutes/protectedAuthRoutes.js
@@ -1,12 +1,18 @@
 import express from "express";
 
 import { requireAuth } from "../../middleware/auth.js";
+import { db } from "../../db.js";
 
 const router = express.Router();
 
 router.get("/verify-token", requireAuth, async (req, res) => {
   try {
-    res.status(200).json({ valid: true, uid: req.user.uid });
+    const [rows] = await db.query(
+      "SELECT uid, firstname, email, created_at, last_login FROM users WHERE uid = ?",
+      [req.user.uid],
+    );
+
+    res.status(200).json({ valid: true, uid: req.user.uid, user: rows[0] });
   } catch (error) {
     console.error("invalid token.:", error);
     res.status(500).json({ message: "invalid token.", error: error.message });

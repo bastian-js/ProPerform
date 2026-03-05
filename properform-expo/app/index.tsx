e6b2f1461b75eea98aad5a8b67c8c351b07a5dfe
cankie7474 <cankayacbon@gmail.com>
2026-03-05 12:44:33 +0100

    move navigation logic from _layout to index

--- properform-expo/app/index.tsx
+++ properform-expo/app/index.tsx
@@ -2,29 +2,27 @@ import { Redirect } from "expo-router";
 import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const DEV_MODE_STATUS = true; // false setzen für normal, true dev mode
+const DEV_MODE_STATUS = false;
 
 export default function Index() {
-  const [ready, setReady] = useState(false);
-  const [finished, setFinished] = useState(false);
+  const [finished, setFinished] = useState<boolean | null>(null);
 
   useEffect(() => {
     (async () => {
       const value = await AsyncStorage.getItem("onboardingFinished");
       setFinished(value === "true");
-      setReady(true);
     })();
   }, []);
 
-  if (!ready) return null;
+  if (finished === null) return null;
 
   if (__DEV__ && DEV_MODE_STATUS) {
     return <Redirect href="/dev-menu" />;
   }
 
-  if (!finished) {
-    return <Redirect href="/(onboarding)/OnboardingScreen" />;
-  }
-
-  return <Redirect href="/(tabs)/HomeScreen" />;
+  return finished ? (
+    <Redirect href="/(tabs)/HomeScreen" />
+  ) : (
+    <Redirect href="/(onboarding)/OnboardingScreen" />
+  );
 }
